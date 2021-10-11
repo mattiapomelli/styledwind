f560fbe1fe63570185ec5e8fd77dae99dc833e5a
Mattia Pomelli <mattiapomelli@gmail.com>
2021-10-11 19:31:58 +0200

    Added tailwind css

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import './tailwind.output.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1>Hello</h1>
+    <h1 className="text-4xl font-bold">Hello</h1>
   </React.StrictMode>,
   document.getElementById('root')
 )
