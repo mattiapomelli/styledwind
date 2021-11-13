bf42f9c0254428a8e19d4b4ef4c538b1ff5bf3d2
Mattia Pomelli <mattiapomelli@gmail.com>
2021-11-13 14:48:29 +0100

    passing config instead of class

--- src/styledwind.tsx
+++ src/styledwind.tsx
@@ -5,24 +5,31 @@ type StyledWindComponent<P> = React.ForwardRefExoticComponent<
   React.PropsWithoutRef<P> & React.RefAttributes<HTMLElement>
 >
 
+type Config = {
+  className: string
+}
+
 function sw<P>(
   type: React.ComponentType<P>,
-  className: string
+  config: Config
 ): StyledWindComponent<P>
 
 function sw<K extends keyof JSX.IntrinsicElements>(
   type: K,
-  className: string
+  config: Config
 ): StyledWindComponent<JSX.IntrinsicElements[K]>
 
 function sw<P>(
   type: ElementType | keyof JSX.IntrinsicElements,
-  className: string
+  config: Config
 ): StyledWindComponent<P> {
   return React.forwardRef<HTMLElement, P>((props, ref) => {
     return React.createElement(type, {
       ...props,
-      className,
+      // @ts-ignore:next-line
+      className: `${props?.className ? props?.className : ''} ${
+        config.className
+      }`,
       ref,
     })
   })
@@ -32,7 +39,7 @@ type BaseSw = typeof sw
 
 type SwFunctions = {
   [K in keyof JSX.IntrinsicElements]: (
-    className: string
+    config: Config
   ) => StyledWindComponent<JSX.IntrinsicElements[K]>
 }
 
@@ -42,7 +49,7 @@ const enhancedSw = sw as EnhancedSw
 
 domElements.forEach(<K extends keyof JSX.IntrinsicElements>(domElement: K) => {
   // @ts-ignore:next-line
-  enhancedSw[domElement] = (className: string) => sw(domElement, className)
+  enhancedSw[domElement] = (config: Config) => sw(domElement, config)
 })
 
 export default enhancedSw
